49e0aebc968847f892419fcae2f253cbca0bc85f
Samuel Beek <ik@samuelbeek.com>
2014-06-13 12:15:45 +0200

    Initial Commit

---
+++ quickReceipt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quickReceipt
+//
+//  Created by Samuel Beek on 13-06-14.
+//  Copyright (c) 2014 subtiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
