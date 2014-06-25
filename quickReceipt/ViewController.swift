653bc366111b382a87a6592147669f16e1ec1e5f
Samuel Beek <ik@samuelbeek.com>
2014-06-25 14:58:54 +0200

    next version
    bit further

--- quickReceipt/ViewController.swift
+++ quickReceipt/ViewController.swift
@@ -8,34 +8,68 @@
 
 import UIKit
 import AVFoundation
+import QuartzCore
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, DBRestClientDelegate {
    
     //properties:
-    
-    @IBOutlet var imageView : UIImageView = nil
-    
-    
     var restClient: DBRestClient = DBRestClient()
-    var session: AVCaptureSession = AVCaptureSession()
+    var captureSession: AVCaptureSession = AVCaptureSession()
+    var device: AVCaptureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+    var captureVideoPreviewLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer()
+    
+    var viewLayer: CALayer = CALayer()
+    @IBOutlet var cameraView: UIView
+    
     
     override func viewDidLoad() {
-        super.viewDidLoad()
         println("viewDidLoad")
 
+        //setting devices
+        
+        var error: NSErrorPointer = nil
+        
+        var input: AVCaptureDeviceInput = AVCaptureDeviceInput.deviceInputWithDevice(device, error: error) as AVCaptureDeviceInput
+        
+        if input == nil {
+            println("something went wrong")
+        }
+        
+        var output:AVCaptureVideoDataOutput = AVCaptureVideoDataOutput()
+        output.videoSettings = nil
+        var queue: dispatch_queue_t = dispatch_queue_create("MyQue", nil)
+        //queue that stuff here
+        
+        
+        
         //setting delegates
         restClient = DBRestClient(session: DBSession.sharedSession())
         restClient.delegate = self
         
+        //making the previewlayer the same size as the cameraView
+        viewLayer.frame = cameraView.frame
+        viewLayer.backgroundColor = CGColorCreateGenericRGB(0.5, 0.7,0.2,0.9)
+        
+//        cameraView.layer.addSublayer(viewLayer)
+        
         //set camera
-        if session.canSetSessionPreset(AVCaptureSessionPresetPhoto){
+        if captureSession.canSetSessionPreset(AVCaptureSessionPreset1280x720){
             println("photo is possible")
-            session.sessionPreset = AVCaptureSessionPresetPhoto
+            captureSession.sessionPreset = AVCaptureSessionPreset1280x720
+            
+            var captureVideoPreviewLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            cameraView.layer.addSublayer(captureVideoPreviewLayer)
+            captureSession.addInput(input)
+            captureSession.addOutput(output)
+
+            
         } else {
             println("can't take no photos")
         }
 
-        session.startRunning()
+        captureSession.startRunning()
+        super.viewDidLoad()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,24 +77,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Dispose of any resources that can be recreated.
     }
     
-
-
-    @IBAction func takePhoto(sender : AnyObject) {
-        println("takePhoto")
-        
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
-        {
-            var imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
-//            imagePicker.mediaTypes = [kUTTypeImage]
-            imagePicker.allowsEditing = false
-            
-            self.presentViewController(imagePicker, animated: true, completion: nil)
-        }
-        
-        
-    }
     
     
     @IBAction func didPressLink(){
@@ -94,13 +110,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     
-    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!) {
-        
-        let selectedImage : UIImage = image
-        
-        self.imageView.image = image
-        picker.dismissModalViewControllerAnimated(true)
-    }
     
     //MARK: dropbox calls
     func restClient(client: DBRestClient, uploadedFile desPath:NSString, from srcPath:NSString, metadata:DBMetadata){
@@ -113,6 +122,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         println("File upload failed with error: %@", error)
         
     }
+    
+    //function
 
 
 
